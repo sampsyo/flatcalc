76f927ae9258fd5468806d252fe4843e8497ed9a
Adrian Sampson <adrian@radbox.org>
2023-03-09 14:22:43 -0800

    Remove old parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use pest::{iterators::Pair, Parser};
 use rand::Rng;
 use std::env;
 use std::io;
-use std::io::Read;
-
-#[derive(pest_derive::Parser)]
-#[grammar = "syntax.pest"]
-struct Syntax;
 
 #[derive(Debug)]
 enum BinOp {
@@ -23,30 +17,6 @@ enum Expr {
 }
 
 impl Expr {
-    fn parse(tree: Pair<Rule>) -> Self {
-        match tree.as_rule() {
-            Rule::addExpr | Rule::mulExpr => {
-                let mut pairs = tree.into_inner();
-                let lhs = pairs.next().unwrap();
-                let op = pairs.next().unwrap();
-                let rhs = pairs.next().unwrap();
-                let op = match op.as_rule() {
-                    Rule::add => BinOp::Add,
-                    Rule::sub => BinOp::Sub,
-                    Rule::mul => BinOp::Mul,
-                    Rule::div => BinOp::Div,
-                    _ => unreachable!(),
-                };
-                Expr::Binary(op, Box::new(Expr::parse(lhs)), Box::new(Expr::parse(rhs)))
-            }
-            Rule::number => {
-                let num = tree.as_str().parse().unwrap();
-                Expr::Literal(num)
-            }
-            _ => unreachable!(),
-        }
-    }
-    
     fn interp(&self) -> i64 {
         match self {
             Expr::Binary(op, lhs, rhs) => {
