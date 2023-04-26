2ecfe4e3abf7ebdb8e8f9f2218679a044877bb09
Adrian Sampson <adrian@radbox.org>
2023-04-26 07:26:15 -0700

    Add "full" benchmark script

---
+++ bench.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+seed=12345
+hyperfine --export-csv bench.csv -w1 \
+  --prepare 'git checkout main ; cargo build --release' \
+    -n 'normal' "./target/release/flatcalc gen_interp $seed" \
+  --prepare 'git checkout main ; cargo build --release --features nofree' \
+    -n 'normal nofree' "./target/release/flatcalc gen_interp $seed" \
+  --prepare 'git checkout flat ; cargo build --release' \
+    -n 'flat' "./target/release/flatcalc gen_interp $seed" \
+  --prepare 'git checkout flat ; cargo build --release --features nofree' \
+    -n 'flat nofree' "./target/release/flatcalc gen_interp $seed" \
+  --prepare 'git checkout flat ; cargo build --release' \
+    -n 'extra-flat' "./target/release/flatcalc gen_flat_interp $seed" \
+  --prepare 'git checkout flat ; cargo build --release --features nofree' \
+    -n 'extra-flat nofree' "./target/release/flatcalc gen_flat_interp $seed"
